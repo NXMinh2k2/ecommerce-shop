3469b33b14a4dc1f09f24667f80d472e97b27719
NXMinh2k2 <minhxuannguyen0809@gmail.com>
2022-05-22 17:30:35 +0700

    Tạo Layout và Header

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Link, Route} from 'react-router-dom'
+import Router from '../routes/Router'
+import Footer from './Footer'
+import Header from './Header'
+
+const Layout = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Router />
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default Layout
